53dfb03c56b590e39d86bb638f9e66ba0cd2e7b1
Raspberry_Pi Pakistan <36759636+SyedUmaidAhmed@users.noreply.github.com>
2018-02-23 17:25:06 +0500

    Update pir_sensor.py

--- pir_sensor.py
+++ pir_sensor.py
@@ -1,6 +1,6 @@
 from gpiozero import MotionSensor
-furrukh = MotionSensor(14)
+pir = MotionSensor(14)
 while True:
-    if furrukh.motion_detected:
-        print('zafeer ny ungli krdi')
+    if pir.motion_detected:
+        print('Intruder is here')
         
